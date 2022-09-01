3bac8f502c426f63e63392b217b560c494c3e2a0
arianafailoc <[200~arianafailoccastro@outlook.com>
2022-09-01 08:18:04 -0500

    sumando enteros

---
+++ semana 2/sumandoenteros.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    a = 20;
+    int b;
+    b = 10;
+
+    int suma = a + b ;
+
+    cout<<"el resultado de la suma es: "<<suma<<endl;
+
+    return 0;
+
+}
